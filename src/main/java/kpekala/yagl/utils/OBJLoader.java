ae657b404df6681bb920cc776bd911799fc9b113
Konrad Pękala <kgpekala@gmail.com>
2023-04-16 01:06:34 +0200

    comments

--- src/main/java/kpekala/yagl/utils/OBJLoader.java
+++ src/main/java/kpekala/yagl/utils/OBJLoader.java
@@ -1,9 +1,9 @@
 package kpekala.yagl.utils;
 
 
+import com.mokiat.data.front.parser.*;
 import kpekala.yagl.yagl.model.complex.Polygon;
 import kpekala.yagl.yagl.utils.ColorUtils;
-import com.mokiat.data.front.parser.*;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,11 +12,32 @@ import java.text.MessageFormat;
 import java.util.List;
 
 public class OBJLoader {
+
+    //Example of use
+    public static void main(String[] args) {
+        URL objFileURL = OBJLoader.class.getResource("/sample.obj");
+        if (objFileURL != null) {
+            try (InputStream in = objFileURL.openStream()) {
+                // Create an OBJParser and parse the resource
+                final IOBJParser parser = new OBJParser();
+                final OBJModel model = parser.parse(in);
+
+                // Use the model representation to get some basic info
+                System.out.println(MessageFormat.format(
+                        "OBJ model has {0} vertices, {1} normals, {2} texture coordinates, and {3} objects.",
+                        model.getVertices().size(),
+                        model.getNormals().size(),
+                        model.getTexCoords().size(),
+                        model.getObjects().size()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public static Polygon[] load(String objPath, String mtlPath) throws IOException {
         var objModel = loadObjModel(objPath);
         var objToDraw = objModel.getObjects().get(0);
-
-//        var mtlLibrary = loadMtlLibrary(mtlPath);
         var singleMesh = objToDraw.getMeshes().get(0);
         return parseMesh(singleMesh, objModel.getVertices(), null);
     }
@@ -40,39 +61,14 @@ public class OBJLoader {
 
     private static Polygon[] parseMesh(OBJMesh mesh, List<OBJVertex> vs, MTLLibrary mtlLibrary) {
         List<OBJFace> faces = mesh.getFaces();
-//        var material = mtlLibrary.getMaterial(mesh.getMaterialName());
-//        var aColor = material.getAmbientColor();
-//        var color = new Vector3f(aColor.r,aColor.g,aColor.b);
         var polygons = new Polygon[faces.size()];
-        for (int i=0; i<faces.size(); i++){
+        for (int i = 0; i < faces.size(); i++) {
             var color = ColorUtils.randomGreyColor();
             float[][] data = faces.get(i).getReferences().stream().
-                    map(ref -> new float[]{vs.get(ref.vertexIndex).x,vs.get(ref.vertexIndex).y,vs.get(ref.vertexIndex).z})
+                    map(ref -> new float[]{vs.get(ref.vertexIndex).x, vs.get(ref.vertexIndex).y, vs.get(ref.vertexIndex).z})
                     .toArray(float[][]::new);
             polygons[i] = new Polygon(data, color);
         }
         return polygons;
     }
-
-    public static void main(String[] args) {
-        URL objFileURL = OBJLoader.class.getResource("/sample.obj");
-        if (objFileURL != null) {
-            try (InputStream in = objFileURL.openStream()) {
-                // Create an OBJParser and parse the resource
-                final IOBJParser parser = new OBJParser();
-                final OBJModel model = parser.parse(in);
-
-                // Use the model representation to get some basic info
-                System.out.println(MessageFormat.format(
-                        "OBJ model has {0} vertices, {1} normals, {2} texture coordinates, and {3} objects.",
-                        model.getVertices().size(),
-                        model.getNormals().size(),
-                        model.getTexCoords().size(),
-                        model.getObjects().size()));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
 }
