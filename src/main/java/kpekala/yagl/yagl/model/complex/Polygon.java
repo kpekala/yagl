7a990c61ba41bd36fcd803678f50b67ec79f9bd0
Konrad Pekala <konrad.pekala@motorolasolutions.com>
2023-10-06 12:10:34 +0200

    chore: remove n field

--- src/main/java/kpekala/yagl/yagl/model/complex/Polygon.java
+++ src/main/java/kpekala/yagl/yagl/model/complex/Polygon.java
@@ -13,38 +13,35 @@ public class Polygon {
     public Vector2f[] coefs;
     public float yMin, yMax;
 
-    private int n;
     private float[] planeCoefs = new float[4];
 
     private Vector3f color;
 
 
-    public Polygon(Polygon that){
+    public Polygon(Polygon that) {
         this.vertices = that.vertices.clone();
         this.yMax = that.yMax;
         this.yMin = that.yMin;
         this.coefs = that.coefs.clone();
-        this.n = that.n;
         this.planeCoefs = that.planeCoefs;
         this.color = that.color;
     }
 
-    public Polygon(float[][] data){
+    public Polygon(float[][] data) {
         this.vertices = new Vector3f[data.length];
-        for(int i=0; i<data.length; i++){
-            vertices[i] = new Vector3f(data[i][0],data[i][1],data[i][2]);
+        for (int i = 0; i < data.length; i++) {
+            vertices[i] = new Vector3f(data[i][0], data[i][1], data[i][2]);
         }
         init();
     }
 
-    public Polygon(float [][] data, Vector3f color){
+    public Polygon(float[][] data, Vector3f color) {
         this(data);
         this.color = color;
     }
 
     private void init() {
-        this.n = vertices.length;
-        coefs = new Vector2f[n];
+        coefs = new Vector2f[vertices.length];
 
         findMinAndMax();
         computeCoefficients();
@@ -53,41 +50,41 @@ public class Polygon {
     private void findMinAndMax() {
         yMin = 1000;
         yMax = -1000;
-        for(var vertex: vertices){
-            yMax = Math.max(yMax,vertex.y);
+        for (var vertex : vertices) {
+            yMax = Math.max(yMax, vertex.y);
             yMin = Math.min(yMin, vertex.y);
         }
     }
 
     private void computeCoefficients() {
-        for(int i=0; i< vertices.length; i++){
+        for (int i = 0; i < vertices.length; i++) {
             Vector3f v1 = vertices[i];
-            Vector3f v2 = vertices[(i+1)% vertices.length];
-            float a = (v2.y-v1.y)/(v2.x-v1.x);
-            float b = v1.y - a*v1.x;
-            coefs[i] = new Vector2f(a,b);
+            Vector3f v2 = vertices[(i + 1) % vertices.length];
+            float a = (v2.y - v1.y) / (v2.x - v1.x);
+            float b = v1.y - a * v1.x;
+            coefs[i] = new Vector2f(a, b);
         }
-        planeCoefs = Maths.planeCoefficients(vertices[0], vertices[1],vertices[2]);
+        planeCoefs = Maths.planeCoefficients(vertices[0], vertices[1], vertices[2]);
     }
 
-    public float zValueAtPoint(float x, float y){
-        return Maths.zPlaneValue(planeCoefs,x,y);
+    public float zValueAtPoint(float x, float y) {
+        return Maths.zPlaneValue(planeCoefs, x, y);
     }
 
-    public void update(){
+    public void update() {
         findMinAndMax();
         computeCoefficients();
     }
 
-    public List<Float> findIntersections(int yHeight){
+    public List<Float> findIntersections(int yHeight) {
         ArrayList<Float> xs = new ArrayList<>();
-        for(int i=0; i< vertices.length; i++){
-            if(isIntersectingWithEdge(i,yHeight)){
-                if (coefs[i].x == 0){
+        for (int i = 0; i < vertices.length; i++) {
+            if (isIntersectingWithEdge(i, yHeight)) {
+                if (coefs[i].x == 0) {
                     xs.add(vertices[i].x);
-                }else{
+                } else {
                     if (Float.isFinite(coefs[i].x))
-                        xs.add(Maths.fParameter(coefs[i].x,coefs[i].y,yHeight));
+                        xs.add(Maths.fParameter(coefs[i].x, coefs[i].y, yHeight));
                     else
                         xs.add(vertices[i].x);
                 }
@@ -97,9 +94,9 @@ public class Polygon {
         return xs;
     }
 
-    private boolean isIntersectingWithEdge(int edgeIndex, int y){
+    private boolean isIntersectingWithEdge(int edgeIndex, int y) {
         Vector3f v1 = vertices[edgeIndex];
-        Vector3f v2 = vertices[(edgeIndex+1)%n];
+        Vector3f v2 = vertices[(edgeIndex + 1) % vertices.length];
         return (y >= v1.y && y <= v2.y) || (y >= v2.y && y <= v1.y);
     }
 
